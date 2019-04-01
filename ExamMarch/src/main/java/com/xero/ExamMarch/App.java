aa9705a7fcd6440f3b9254c26c4c9f040629ef91
Tulika <tulikapandey16@gmail.com>
2019-04-01 12:16:11 -0500

    Xero Exam

---
+++ ExamMarch/src/main/java/com/xero/ExamMarch/App.java
@@ -0,0 +1,13 @@
+package com.xero.ExamMarch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ExamMarch/src/test/java/com/xero/ExamMarch/ReuseMethods.java
@@ -0,0 +1,25 @@
+package com.xero.ExamMarch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ReuseMethods {
+
+	static WebDriver driver;
+	
+	/* Name of the Method: 	clickObject
+	 * Brief Description: 	Clicking the Button
+	 * Arguments: 			WebElement
+	 * Created by:			Tulika
+	 * Created Date:		03/29/2019
+	 * Last Modified:		03/29/2019
+	 */
+
+	public static void clickObject(WebElement element) throws InterruptedException{
+	element.click();
+			
+	}
+}
+
+
+
